5e41f0442100cf6915f90b63b3076fd2ca2c0bae
Dave Garred <dave.garred@gmail.com>
2025-01-06 09:58:53 -0800

    Deprecate this repository

--- src/types.rs
+++
@@ -1,8 +0,0 @@
-use cqrs_es::persist::PersistedEventStore;
-use cqrs_es::CqrsFramework;
-
-use crate::DynamoEventRepository;
-
-/// A convenience type for a CqrsFramework backed by
-/// [DynamoStore](struct.DynamoStore.html).
-pub type DynamoCqrs<A> = CqrsFramework<A, PersistedEventStore<DynamoEventRepository, A>>;
